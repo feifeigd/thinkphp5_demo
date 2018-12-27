36a851af67797c0f3f126130bbb65ca80edc539c
feifeigd <feifeigd@21cn.com>
2018-12-27 13:10:11 +0800

    添加测试

---
+++ tests/IndexTest.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: luo fei
+ * Date: 2018/12/26
+ * Time: 14:20
+ */
+
+namespace tests;
+
+
+class IndexTest extends TestCase
+{
+  public function testSomethingIsTrue(){
+    $this->assertTrue(true);
+  }
+}
