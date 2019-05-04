ac228fac97dddf92fc8872d31747ced5db26d692
feifeigd <feifeigd@21cn.com>
2019-05-04 18:27:18 +0800

    get_php_system config.php

--- application/install/common.php
+++ application/install/common.php
@@ -14,6 +14,28 @@ function is_write($file){
     return $F->is_write($file);
 }
 
+/// 判断系统类模块函数是否被支持
+/// @param $data array 系统设置参数
+/// @return array
+function get_php_system($data){
+    if(!is_array($data) || count($data) <= 0) return [];
+    $res = [];
+    foreach($data as $k => $v){
+        if(!$v)continue;
+        if(is_callable($v)){
+            $param = ['key'=>$k];
+            $res[] = $v[$param];
+        }elseif($v == 'class')
+            $res[] = [$k, '类', class_exists($k)];
+        elseif($v == 'function')
+            $res[] = [$k, '函数', function_exists($k)];
+        elseif($v == 'model')
+            $res[] = [$k, '模块', extension_loaded($k)];
+        else $res[] = [$k, '未知', false];
+    }
+    return $res;
+}
+
 /// 判断文件或者文件夹是否有读写权限
 /// @param $data array 系统设置参数
 /// @return array
@@ -27,3 +49,16 @@ function is_write_array($data){
     return $res;
 }
 
+/// 环境检测
+/// @param $data array 系统设置参数
+/// @return array
+function is_system($data){
+    if(!is_array($data) || count($data) <= 0) return [];
+    $res = [];
+    foreach ($data as $v){
+        if(!$v || !is_array($v))continue;
+        $is = is_callable($v[2]) ? $v[2]($v[1]) : $v[2];
+        $res[] = [$v[0], $v[1], $is];
+    }
+    return $res;
+}
--- config/install/config.php
+++ config/install/config.php
@@ -5,3 +5,57 @@
  * Date: 2019/5/4
  * Time: 12:17
  */
+
+return [
+    'content'   => [
+        'env_items' => [
+            // '检测环境变量名称' => ['说明文字', '需要值', '检测方法']
+            // 检测方法返回数组 ['status' => 是否通过(boolean)， 'rightnow'=>当前状态显示]
+            'os'    => ['操作系统', 'WINNT|Linux', function($system = 'WINNT|Linux'){
+                $array['rightnow'] = PHP_OS;
+                $system = explode('|', $system);
+                if(!is_array($system)){
+                    $array['status'] = (PHP_OS == $system);
+                }else $array['status'] = in_array(PHP_OS, $system);
+                return $array;
+            }],
+            'php'   => ['php版本', '5.6.0', function(){
+                $array['rightnow'] = PHP_VERSION;
+                $array['status'] = version_compare(PHP_VERSION, '5.6.0', '>=');
+                return $array;
+            }],
+            'attachmentupload'  => ['附件上传', '可用', function(){
+                if(@ini_get['file_uploads']){
+                    $array['rightnow'] = ini_get('upload_max_filesize');
+                    $array['status'] = true;
+                }else{
+                    $array['rightnow'] = '不可用';
+                    $array['status'] = false;
+                }
+                return $array;
+            }],
+            'gdversion' => ['GD扩展', '可用', function(){
+                if(extension_loaded('gd')){
+                    $tmp = gd_info();
+                    $array['rightnow'] = $tmp['GD Version'] ?: '';
+                    $array['status'] = true;
+                    unset($tmp);
+                }else{
+                    $array['rightnow'] = '不可用';
+                    $array['status'] = false;
+                }
+                return $array;
+            }],
+        ],
+        'dir_items'  => [
+            'application/install',
+        ],
+
+        'func_items'    => [
+            'file_get_contents' =>  'function',
+            'curl_init' => 'function',
+            'mb_strlen' => 'function',
+            'mysqli'    => 'class',
+        ],
+    ],
+];
