78c707caff084c5731fe61a0343771ab966668b1
feifeigd <feifeigd@21cn.com>
2019-05-04 18:51:03 +0800

    step 1

--- application/install/controller/Index.php
+++ application/install/controller/Index.php
@@ -9,7 +9,47 @@
 namespace app\install\controller;
 
 
-class Index
+use files\File;
+use think\Controller;
+use think\facade\Request;
+
+class Index extends Controller
 {
+    public function index(){
+        $root_path = env('root_path');
+        $file = new File();
+        if($file->f_has($root_path.'application/install/install.install')) die('程序已经安装，如重新安装请到install模块根目录下删除install.install');
+        $step = Request::param('step') ?: 1;
+        if(!session('step') || (session('step') + 1) < $step){
+            session('step', $step);
+            echo "<script>location.href='".url('install/index/index/', ['step'=>$step])."'</script>";
+        }else
+            session('step', $step);
+        $config = config();
+        switch ($step){
+            case '1':   // 安装第一步,阅读安装协议
+                $content = lang('Agreement');
+                $this->assign('content', $content);
+                $pagename = lang('step0');
+                $this->assign('pagename', $pagename);
+                return $this->fetch('install');
+                break;
+            case '2':   // 安装第二步,检测环境
+                $pagename = lang('step1');
+                $config = $config['config']['content']; // config.php.content
+                $env_items = is_system($config['env_items']);
+                $dir_items = is_write_array($config['dir_items']);
+                $func_items = get_php_system($config['func_items']);
+                $this->assign([
+                    'pagename'      => $pagename,
+                    'env_items'     => $env_items,
+                    'dir_items'     => $dir_items,
+                    'func_items'    => $func_items,
+                ]);
+                return $this->fetch('step1');
+                break;
+        }
+        return $root_path;
+    }
 
 }
--- application/install/lang/zh-cn.php
+++ application/install/lang/zh-cn.php
@@ -18,9 +18,30 @@ return [
     'next'  => '下一步',
 
     // 安装检测页面
-
-
+    'step1_runtime'         => '运行环境检查',
+    'step1_system'          => '项目',
+    'step1_required_need'   => '所需配置',
+    'step1_required_now'    => '当前配置',
+    'step1_dependency'      => '依赖性',
+    'step1_name'            => '名称',
+    'step1_type'            => '类型',
+    'step1_result'          => '检查结果',
+    'step1_permissions'     => '目录，文件权限检查',
+    'step1_files'           => '目录/文件',
+    'step1_required_state'  => '所需状态',
+    'step1_now_state'       => '当前状态',
+    'step1_not_write'       => '不可写',
+    'step1_write'           => '可写',
+    'os'                    => '操作系统',
+    'php'                   => 'php版本',
+    'attachmentupload'      => '附件上传',
+    'gdversion'             => 'GD库',
+    'diskspace'             => '磁盘空间',
+    'installopen'           => '开启',
+    'unlimited'             => '不限制',
     // 安装协议
     'Agreement' => '安装协议',
+
+    // 数据库安装
 
 ];
